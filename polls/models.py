a0fc876eb142b835a94bb455358cbf9d33eb5db7
muminoff <smuminov@gmail.com>
2016-05-18 22:02:42 +0900

    Added: first poll model and its testcase

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(
+            _("Title"),
+            max_length=100,
+            null=False,
+            blank=False)
+    description = models.TextField()
+    creator = models.OneToOneField(
+            User,
+            verbose_name=_("Creator"))
+    created_time = models.DateTimeField(
+            auto_now_add=True)
+    modified_time = models.DateTimeField(
+            auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'polls'
+        ordering = ['-created_time']
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from model_mommy import mommy
+from polls.models import Poll
+from random import choice as random_choice
 
-# Create your tests here.
+
+class TestCase(TestCase):
+
+    def setUp(self):
+        self.users = mommy.make(User, _quantity=10)
+        self.polls = mommy.make(Poll, _quantity=100)
+
+    def tearDown(self):
+        del self.users
+        del self.polls
+
+    def test_users_count(self):
+        self.assertEqual(
+                len(self.users), 10)
+
+    def test_polls_count(self):
+        self.assertEqual(
+                len(self.polls), 100)
+
+    def test_random_change(self):
+        random_poll = random_choice(self.polls)
+        self.assertIsInstance(random_poll, Poll)
+
+        random_poll.title = 'foobar'
+        random_poll.description = 'foobar'
+        self.assertEqual(random_poll.title, 'foobar')
+        self.assertEqual(random_poll.description, 'foobar')
